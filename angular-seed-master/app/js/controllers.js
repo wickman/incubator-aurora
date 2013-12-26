07230b84284d4f85b2f81d45356f18b7a671bdd4
Suman Karumuri <skarumuri@twitter.com>
2013-12-26 14:14:02 -0800

    Added a page header.

--- angular-seed-master/app/js/controllers.js
+++ angular-seed-master/app/js/controllers.js
@@ -5,5 +5,5 @@
 angular.module('auroraUI.controllers', []).
   controller('AuroraUI.JobSummaryController',
     function($scope) {
-      $scope.summary = 'Hello World!';
+      $scope.title = 'Scheduled Jobs Summary';
   });
