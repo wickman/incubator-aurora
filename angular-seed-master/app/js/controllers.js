ae809dffdfe8860665fb6f12f249ef31f24097ad
Suman Karumuri <skarumuri@twitter.com>
2013-12-27 11:41:13 -0800

    Removed ng-grid.

--- angular-seed-master/app/js/controllers.js
+++ angular-seed-master/app/js/controllers.js
@@ -9,7 +9,7 @@ angular.module('auroraUI.controllers', []).
 
       $scope.jobSummary = { 'jobSummaries': [
         {
-          'role': 'mesos',
+          'role': "mesos",
           'jobs': 10,
           'cronJobs': 10
         },
@@ -19,8 +19,6 @@ angular.module('auroraUI.controllers', []).
           'cronJobs': 20
         }
       ]};
-
-      $scope.jobsummaries = $scope.jobSummary.jobSummaries;
 
       $scope.columnCollection = [
         {label : 'role', map: 'role'},
@@ -33,31 +31,4 @@ angular.module('auroraUI.controllers', []).
       $scope.globalConfig = {
         isGlobalSearchActivated: true
       };
-
-      $scope.filterOptions = {
-        filterText: "",
-        useExternalFilter: false
-      };
-
-      $scope.gridOptions = {
-        data: 'jobsummaries',
-        enablePaging: true,
-        pagingOptions: {
-          pageSizes: [10, 25, 50, 1000],
-          pageSize: 1,
-          currentPage: 1
-        },
-        showFooter: true,
-        enableCellSelection: false,
-        enableRowSelection: false,
-        filterOptions: 'filterOptions',
-        showFilter: true
-      };
-
-      // TODO: ng-Grid improvements:
-      // Fix the search box location in the grid.
-      // Fix the pagination display style..
-      // Show all in page size display.
-      // header instead of footer.
-      // Order of pagination.
-    });
+   });
