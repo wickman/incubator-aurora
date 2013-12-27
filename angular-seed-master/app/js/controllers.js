60756882ba0c55e419f957825af068c3dd9f11e4
Suman Karumuri <skarumuri@twitter.com>
2013-12-26 19:47:16 -0800

    add a table with search. Some css tweaks needed.

--- angular-seed-master/app/js/controllers.js
+++ angular-seed-master/app/js/controllers.js
@@ -6,6 +6,7 @@ angular.module('auroraUI.controllers', []).
     controller('AuroraUI.JobSummaryController',
     function ($scope) {
       $scope.title = 'Scheduled Jobs Summary';
+
       $scope.jobSummary = { 'jobSummaries': [
         {
           'role': 'mesos',
@@ -18,10 +19,16 @@ angular.module('auroraUI.controllers', []).
           'cronJobs': 20
         }
       ]};
+
       $scope.columnCollection = [
         {label : 'role', map: 'role'},
         {label : 'jobs', map: 'jobs'},
         {label : 'cronJobs', map: 'cronJobs'}
       ];
+
       $scope.rowCollection = $scope.jobSummary.jobSummaries;
+
+      $scope.globalConfig = {
+        isGlobalSearchActivated: true
+      };
     });
