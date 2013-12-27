c0e16eba62a4e0f17fb139aa1496504b29168699
Suman Karumuri <skarumuri@twitter.com>
2013-12-26 19:31:25 -0800

    Displaying dummy data using smaty table.

--- angular-seed-master/app/js/controllers.js
+++ angular-seed-master/app/js/controllers.js
@@ -3,7 +3,25 @@
 /* Controllers */
 
 angular.module('auroraUI.controllers', []).
-  controller('AuroraUI.JobSummaryController',
-    function($scope) {
+    controller('AuroraUI.JobSummaryController',
+    function ($scope) {
       $scope.title = 'Scheduled Jobs Summary';
-  });
+      $scope.jobSummary = { 'jobSummaries': [
+        {
+          'role': 'mesos',
+          'jobs': 10,
+          'cronJobs': 10
+        },
+        {
+          'role': 'ads',
+          'jobs': 101,
+          'cronJobs': 20
+        }
+      ]};
+      $scope.columnCollection = [
+        {label : 'role', map: 'role'},
+        {label : 'jobs', map: 'jobs'},
+        {label : 'cronJobs', map: 'cronJobs'}
+      ];
+      $scope.rowCollection = $scope.jobSummary.jobSummaries;
+    });
