acf8ef2abb6ad576648ba9cdea716d1f67743066
Suman Karumuri <skarumuri@twitter.com>
2013-12-27 00:13:48 -0800

    Removed smart-table code.

--- angular-seed-master/app/js/app.js
+++ angular-seed-master/app/js/app.js
@@ -1,4 +1,4 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-var auroraUI = angular.module('auroraUI', ['auroraUI.controllers', 'smartTable.table', 'ngGrid']);
+var auroraUI = angular.module('auroraUI', ['auroraUI.controllers', 'ngGrid']);
