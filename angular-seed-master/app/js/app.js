a20de364b9341758861c4955c8c98a04e71efdea
Suman Karumuri <skarumuri@twitter.com>
2013-12-30 17:47:12 -0800

    Removing angular-seed dir.

--- angular-seed-master/app/js/app.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-// Declare app level module which depends on filters, and services
-var auroraUI = angular.module('auroraUI', ['auroraUI.controllers', 'smartTable.table']);
--- angular-seed-master/app/js/directives.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-auroraUI.directive('roleLink', function () {
-  return {
-    restrict: 'C',
-    template: "<a ng-href='/{{formatedValue}}'>{{formatedValue}}</a>"
-  };
-});
